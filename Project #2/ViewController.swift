6af7b6d58dd807e0b3d0007925a41239441bfafd
Jaleeljs <35237438+Jaleeljs@users.noreply.github.com>
2018-03-17 18:17:12 -1000

    Initial Commit

---
+++ Project #2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project #2
+//
+//  Created by Xanda-Li Kinney-Kawai on 3/17/18.
+//  Copyright © 2018 jaleeljs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
